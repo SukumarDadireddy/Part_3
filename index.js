c65ca1917ea1d7e101e839b725a0acbcac547568
SUKUMAR REDDY DADIREDDY <dadireddysukumar@gmail.com>
2025-02-28 10:39:23 +0530

    added database

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+require('dotenv').config()
+const PhoneBook = require('./models/Phone')
 var morgan = require('morgan')
 const cors = require('cors')
+const Phone = require('./models/Phone')
 const app = express()
 app.use(express.json())
 app.use(cors());
@@ -11,70 +14,57 @@ morgan.token('req-body', (req) => {
 
     return JSON.stringify(req.body);
   }
-  return '-'; // Return '-' for requests without a body
+  return '-'; 
 });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
-let phoneBook =
-[
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/',(request,response)=>response.send("Hello"))
 
 app.get('/api/persons',
     (request,response)=>
        {
-        response.json(phoneBook)
+        PhoneBook.find({}).then
+        (
+          records=>
+          {
+            response.json(records)
+          }
+        )
        }
 )
 
 app.get('/info',
     (request,response)=>
        {
-        const info = `Phonebook has info for ${phoneBook.length} people`
+        const info = `Phonebook has info for ${PhoneBook.length} people`
         const time = new Date().toString();
         response.type('text').send(`${info}\n\n${time}`)
        }
 )
 
 app.get('/api/persons/:id',
-    (request,response)=>
+    (request,response,next)=>
        {
-        const entry = phoneBook.find(e=>e.id===request.params.id)
-        if(entry)
-        {
-        response.json(entry)
-        }
-        else 
-        {
+        PhoneBook.findById(request.params.id).then(
+          note=>{if (note) {
+            response.json(note)
+          } else {
             response.status(404).end()
-        }
+          }}
+      ).catch(error =>next(error))
        }
 )
 
 app.delete('/api/persons/:id',
-    (request,response)=>
+    (request,response,next)=>
        {
-        phoneBook=phoneBook.filter(e=>e.id!==request.params.id)
+        PhoneBook.findByIdAndDelete(request.params.id).then
+        (
+          result=> response.status(204).end()
+        )
+        .catch(
+          error=>(next(error))
+        )
         response.status(202).json({id:request.params.id})
        }
 )
@@ -87,29 +77,50 @@ app.post('/api/persons',
       {
         return response.status(400).json({"error" : "content missing"}) 
       }
-      if(phoneBook.find(e=>e.name===entry.name))
-      {
-        return response.status(400).json({"error" : "name must be unique"}) 
-      }
-
-    
-
-      const record ={
-        id:String(Math.floor(Math.random()*10000)),
-        name:entry.name,
-        number:entry.number,
-       
-      }
-
-      phoneBook.push(record)
-      response.json(record)
+      const Phone = new PhoneBook(
+        {
+          id:String(Math.floor(Math.random()*10000)),
+          name:entry.name,
+          number:entry.number,
+         
+        } 
+      )
+      Phone.save().then(savedPhone=>
+        response.json(savedPhone)
+      )
     }
 
 )
+
+app.put('/api/persons/:id',(request,response,next)=>
+{
+  const body = request.body
+  const update ={name:body.name, number:body.number}
+  PhoneBook.findByIdAndUpdate(request.params.id,update, {new:true})
+  .then(updatedPhone=>
+    response.json(updatedPhone)
+  )
+  .catch(error=>next(error))
+}
+
+)
+
+
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
   }
-  
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler) 
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
